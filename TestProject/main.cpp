61ac7c5f6b89e251e7759280584852e74f335aaa
Emil Shteinberg <74972640+worriedlemon@users.noreply.github.com>
2026-02-10 09:33:51 +0300

    PR #1: Some changes to `main.cpp`
    * Added comment to main.cpp
    * Added print usage
    * Added new message
    * Fixed printHelp condition
    * Return 1 in print help message condition

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -1,7 +1,27 @@
 ﻿#include <iostream>
 
-int main()
+/// @brief Print help message in console
+void printHelp()
 {
-	std::cout << "Hello, CMake Project." << std::endl;
+	std::cout << "Usage:\n"
+		"main [-h] - Print 'Hello, CMake Project'\n"
+		"\n"
+		"Arguments:\n"
+		"-h - print this message";
+}
+
+/// @brief Main function
+/// @returns 0 if successfully exits, 1 otherwise
+int main(int argc, char* argv[])
+{
+	if (argc > 1)
+	{
+		printHelp();
+		return 1;
+	}
+
+	std::cout << "Hello, \033[32mCMake Project\033[0m." << std::endl;
+
+	std::cout << "This is just a test!" << std::endl;
 	return 0;
 }
