09e5c37a1df305217279749715460efdf0d3c127
worriedlemon <emilshteinberg@yandex.ru>
2026-02-10 09:09:06 +0300

    Added new message

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -4,6 +4,8 @@
 /// @returns 0 if successfully exits, 1 otherwise
 int main()
 {
-	std::cout << "Hello, CMake Project." << std::endl;
+	std::cout << "Hello, \033[32mCMake Project\033[0m." << std::endl;
+
+	std::cout << "This is just a test!" << std::endl;
 	return 0;
 }
