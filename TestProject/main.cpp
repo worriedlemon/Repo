b608c8754227b75eec0124e674dc97b6192d4d5e
worriedlemon <emilshteinberg@yandex.ru>
2026-02-10 09:07:49 +0300

    Added print usage

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -1,9 +1,24 @@
 ﻿#include <iostream>
 
+void printHelp()
+{
+	std::cout << "Usage:\n"
+		"main [-h] - Print 'Hello, CMake Project'\n"
+		"\n"
+		"Arguments:\n"
+		"-h - print this message";
+}
+
 /// @brief Main function
 /// @returns 0 if successfully exits, 1 otherwise
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc == 1)
+	{
+		printHelp();
+		return 0;
+	}
+
 	std::cout << "Hello, CMake Project." << std::endl;
 	return 0;
 }
